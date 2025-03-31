ea49fa341ccf319ba00d01209b82d603b20def68
Svyatozar Naumov <svyatozar1@gmail.com>
2025-03-31 18:20:00 +0300

    modified: /app/screens

---
+++ app/screens/lists.tsx
@@ -0,0 +1,15 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function ScannerScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Нежелательные ингредиенты</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    title: { fontSize: 24, fontWeight: 'bold' }
+  });
+  
